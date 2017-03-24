9a1e7e4ec1f231c9b467049204554e3ef7586019
ThangTruong <thangtm6@fsoft.com.vn>
2017-03-25 06:26:17 +0700

    Retain cycles - số object bị deinit không bằng số object được sinh ra

--- retainCycle/ViewController.swift
+++ retainCycle/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class BaseViewController: UIViewController {
+    var previousVC: BaseViewController?
+    var nextVC: BaseViewController?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +21,28 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    deinit {
+        print("\(type(of: self)) - deinit")
+        
+    }
 
+    // MARK: Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        nextVC = segue.destination as? BaseViewController
+        nextVC?.previousVC = self
+    }
 
 }
 
+class VC1 : BaseViewController {
+    
+}
+
+class VC2 : BaseViewController {
+    
+}
+
+class VC3 : BaseViewController {
+    
+}
